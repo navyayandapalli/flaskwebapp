e528596e65b444d1ce636105d8c038ce3ab73ac4
navyayandapalli <navyayandapalli@gmail.com>
2025-02-12 16:52:23 +0000

    Updated app.py

--- movie/app.py
+++ movie/app.py
@@ -1,32 +1,46 @@
 from flask import *
-from flask_mysqldb import MySQL
-app = Flask("mymovieapp")
+#from flask_mysqldb import MySQL
+#from flaskext.mysql import MySQL
+from flask import render_template
+import pymysql
+
+app = Flask("myMovieapp")
 
 app.secret_key ='abcd'
 
-# MySQL Configurations
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root@12'
-app.config['MYSQL_DB'] = 'moviedb'
 
-mysql = MySQL(app)
+app_config ={
+        "host" : '192.168.0.108',
+        "user" : 'root',
+        "password" : 'Root@1234',
+        "database" : 'moviedb'
+}
+
 
 @app.route("/")
 def Index():
     return render_template('index.html')
+
+def get_db_connection():
+    connection = pymysql.connect(**app_config)
+    return connection
+
+@app.route('/')
+def Home():
+    connection = get_db_connection()
 
 @app.route('/add', methods=['POST'])
 def add_movie():
     session["uid"] = request.form.get("id")
     session["uname"] = request.form.get("name")
     session["udescription"] = request.form.get("description")
-
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO movies (id, name, description) VALUES (%s, %s, %s)", 
+    connection = get_db_connection()
+    cur = connection.cursor()
+    cur.execute("INSERT INTO movies (id, name, description) VALUES (%s, %s, %s)",
                 (session["uid"], session["uname"], session["udescription"]))
-    mysql.connection.commit()
+    connection.commit()
     cur.close()
+    connection.close()
     return redirect('/')
 
 @app.route('/add', methods=['GET'])
@@ -36,23 +50,26 @@ def add_movie_form():
 @app.route('/search', methods=['GET', 'POST'])
 def search_movie():
     session["uid"] = request.form.get("id")
-        
-    cur = mysql.connection.cursor()
+    connection = get_db_connection()
+    cur = connection.cursor()
+
     cur.execute("SELECT * FROM movies WHERE id = %s", (session["uid"],))
     movie = cur.fetchone()
     cur.close()
-    
+    connection.close()
+
     return render_template('search.html', movie=movie)
 
 @app.route('/movies')
 def show_movies():
-    cur = mysql.connection.cursor()
+
+    connection = get_db_connection()
+    cur = connection.cursor()
     cur.execute("SELECT * FROM movies")
     movies = cur.fetchall()
     cur.close()
-    
-    return render_template('show.html', movies=movies)    
-  
+    connection.close()
 
+    return render_template('show.html', movies=movies)
 
-app.run(debug=True)
+app.run('0.0.0.0', port=5000, debug=True)
